e28229821975aa14281d82ad3f746368cfb11fc9
Anthony HUREZ <anthonyhurez@yahoo.fr>
2020-10-14 14:53:50 +0200

    Nouveau Snap
    Création des trois caméras, des layers et début du script

---
+++ SNAP/Assets/Scripts/Snap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snap : MonoBehaviour
+{
+    private GameObject[] cameras = new GameObject[3];
+    private float snapPressed;
+    private int actualDimension = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameras = GameObject.FindGameObjectsWithTag("MainCamera");
+        cameras[1].GetComponent<Camera>().enabled = false;
+        cameras[2].GetComponent<Camera>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //snapPressed vaudra -1 avec le bouton '1' et 1 avec le bouton '2'
+        snapPressed = Input.GetAxis("SNAP");
+        if(Input.GetButtonDown("SNAP"))
+        {
+            Debug.Log(snapPressed);
+        }
+    }
+}
