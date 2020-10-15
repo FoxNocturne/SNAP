d3e83ef81ac71229d138647f2dd4283ceb29f589
Anthony HUREZ <anthonyhurez@yahoo.fr>
2020-10-15 14:22:35 +0200

    Vision des policiers
    Les policiers ont un cône de vision mais ils peuvent voir à travers les murs, pour l'instant...

--- SNAP/Assets/Scripts/PolicierAI.cs
+++ SNAP/Assets/Scripts/PolicierAI.cs
@@ -6,14 +6,20 @@ public class PolicierAI : MonoBehaviour
 {
     public float maxSpeed = 5;
     public float TempsDePause = 2;
+    public GameObject player;
+    public float viewDistance;
+    public LayerMask layerMask;
 
     private List<Transform> path = new List<Transform>();
     private int currentTargetIndex = 0;
     private Vector2 currentTargetPos;
     private bool isWaiting = false;
+    private bool directionGauche;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
         Transform pathParent = transform.GetChild(0);
         foreach (Transform child in pathParent)
         {
@@ -37,9 +43,35 @@ public class PolicierAI : MonoBehaviour
             }
             else
             {
+                directionGauche = currentPos.x > currentTargetPos.x;
+
                 float step = maxSpeed * Time.deltaTime;
 
                 transform.position = Vector2.MoveTowards(currentPos, new Vector2(currentTargetPos.x, transform.position.y), step);
+            }
+        }
+
+        if (Vector2.Distance(currentPos, player.transform.position) <= viewDistance)
+        {
+            float angle = Vector2.Angle(transform.right, (player.transform.position - transform.position).normalized);
+
+            if(directionGauche)
+            {
+                if(angle > 135)
+                {
+                    Debug.Log("Le joueur est dans mon cône de vision");
+
+                    if (Physics2D.Raycast(transform.position, Vector2.left, viewDistance, layerMask))
+                    {
+                        Debug.Log("Je le vois !");
+                    }
+                }
+            }else
+            {
+                if(angle < 45)
+                {
+                    Debug.Log("Le joueur est dans mon cône de vision");
+                }
             }
         }
     }
@@ -59,5 +91,16 @@ public class PolicierAI : MonoBehaviour
         {
             Physics2D.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider2D>());
         }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        float angleX = Mathf.Sin(angleInDegrees * Mathf.Deg2Rad) * (directionGauche ? -1 : 1);
+        float angleY = Mathf.Cos(angleInDegrees * Mathf.Deg2Rad) * (directionGauche ? -1 : 1);
+        return new Vector3(angleX, angleY, 0);
     }
 }
