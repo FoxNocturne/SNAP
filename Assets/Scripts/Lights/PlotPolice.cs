08d2e9fc5ad809e10539b7bf220b2576f8793d07
Anthony HUREZ <anthonyhurez@yahoo.fr>
2020-12-07 14:55:15 +0100

    Intégration premier tableau + lumières

---
+++ Assets/Scripts/Lights/PlotPolice.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class PlotPolice : MonoBehaviour
+{
+    public float speed = 5;
+
+    private Light2D lightComp;
+    private float maxRadius;
+    private bool on = true;
+
+    void Start()
+    {
+        lightComp = GetComponent<Light2D>();
+        maxRadius = lightComp.pointLightOuterRadius;
+    }
+    
+    void Update()
+    {
+        if (on)
+        {
+            lightComp.pointLightOuterRadius = Mathf.Clamp(lightComp.pointLightOuterRadius - (speed * Time.deltaTime), 0, maxRadius);
+            on = !(lightComp.pointLightOuterRadius == 0);
+        }
+        else
+        {
+            lightComp.pointLightOuterRadius = Mathf.Clamp(lightComp.pointLightOuterRadius + (speed * Time.deltaTime), 0, maxRadius);
+            on = lightComp.pointLightOuterRadius == maxRadius;
+        }
+    }
+}
