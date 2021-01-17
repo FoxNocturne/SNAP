1fda1e76b835fdfdb1f5006cf10a7b98a6e8a10e
Lionel <bezamanifary.lionel@gmail.com>
2021-01-17 12:30:08 +0100

    Système collectable amélioré
    - Lorsque Mr.X récupère un collectable, la donnée est sauvegardé dans playerprefs.SetInt("nomCollectable", numeroCollectable)
    - Ajout d'une bulle en haut de l'écran indiquant quel objet a été récupéré

--- Assets/Scripts/ObserveThisThing.cs
+++ Assets/Scripts/ObserveThisThing.cs
@@ -6,31 +6,6 @@ using UnityEngine.UI;
 public class ObserveThisThing : MonoBehaviour
 {
     // Ajouter manuellement sur Unity le texte et l'image à afiicher lorsque le personnage interagit avec un panneau ou une affiche
-    public GameObject Observe;
-    public Sprite Object_Picture;
-    public string text;
-
-
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-
-            Debug.Log(Time.deltaTime);
-            // RAMASSER UN OBJET
-            if (Input.GetButtonDown("Attraper"))
-            {
-                if (GameObject.Find("ObserveThisThing") == null)
-                {
-                    GameObject DisplayObject = Instantiate(Observe, transform.position, Quaternion.identity);
-                    DisplayObject.name = "ObserveThisThing";
-                    DisplayObject.transform.GetChild(0).GetComponent<Image>().sprite = Object_Picture;
-                    DisplayObject.transform.GetChild(1).GetComponent<Text>().text = text;
-                    Time.timeScale = 0;
-                }
-            }
-        }
-    }
-
+    public int Numero;
+    public string NomCollectable;
 }
