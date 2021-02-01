0481105eb455afc391c88a85fe3cc24a9b3b86e8
Anthony HUREZ <anthonyhurez@yahoo.fr>
2021-02-01 13:07:56 +0100

    Finition du portail
    • Refonte totale du script de placement
    • Transfert d'objet dans le portail, théoriquement on a même plus besoin des layers de transition

--- Assets/Scripts/Portail.cs
+++ Assets/Scripts/Portail.cs
@@ -6,16 +6,25 @@ public class Portail : MonoBehaviour
 {
     public float attractionForce = 1;
 
-    [HideInInspector] public int targetDimension;
     [HideInInspector] public Portail portailLinked;
 
     Transform draggedObject;
     Vector2 defaultScale;
     float maxDistance, colliderMaxDistance;
-    bool absorption = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.transform == draggedObject)
+        {
+            draggedObject.gameObject.layer = portailLinked.gameObject.layer;
+            draggedObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+            draggedObject.localScale = defaultScale;
+            draggedObject.tag = "Item";
+
+            Destroy(portailLinked.gameObject);
+            Destroy(gameObject);
+        }
+
         if (!(collision.tag == "Item"))
             return;
 
@@ -23,12 +32,6 @@ public class Portail : MonoBehaviour
         draggedObject.GetComponent<Rigidbody2D>().gravityScale = 0;
         defaultScale = draggedObject.localScale;
         maxDistance = Mathf.Abs(Vector2.Distance(draggedObject.position, transform.position));
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        absorption = true;
-        colliderMaxDistance = Mathf.Abs(Vector2.Distance(draggedObject.position, transform.position));
     }
 
     private void Update()
@@ -43,100 +46,14 @@ public class Portail : MonoBehaviour
 
         draggedObject.position = Vector2.MoveTowards(draggedObject.position, transform.position, speed);
         draggedObject.localScale = defaultScale * Mathf.InverseLerp(0, maxDistance, distance);
-
-        if (absorption)
-            transform.localScale *= Mathf.InverseLerp(0, colliderMaxDistance, distance);
     }
 
     void OnDestroy()
     {
+        if (draggedObject == null) return;
         draggedObject.GetComponent<Rigidbody2D>().gravityScale = 1;
         draggedObject.localScale = defaultScale;
+        draggedObject.tag = "Item";
     }
 
-
-
-
-
-
-
-    /* 
-    private List<ObjetTransfert> objectsReceived = new List<ObjetTransfert>(); 
-    private GameObject[] cameras = new GameObject[3]; 
- 
-    AudioSource sonPortailAspire; 
-    public AudioClip[] sonPortailObjet; 
- 
-    private void Start() 
-    { 
-        cameras = GameObject.FindGameObjectsWithTag("MainCamera"); 
-        sonPortailAspire = GetComponent<AudioSource>(); 
-    } 
- 
-    private void OnTriggerEnter2D(Collider2D collision) 
-    { 
-        if (objectsReceived.Find(objet => objet.collider == collision) == null) 
-        { 
-            portailLinked.transferObject(new ObjetTransfert(collision, FindProvenance(collision.transform))); 
-            sonPortailAspire.PlayOneShot(sonPortailObjet[1], 0.1f); 
- 
-            if (collision.tag == "Item") 
-            { 
-                collision.gameObject.layer = targetDimension + 12; 
-                cameras[portailLinked.targetDimension].GetComponent<Camera>().cullingMask |= 1 << targetDimension + 12; // Ajoute le layer de transition au culling mask de la dimension de départ 
-            } 
-            else if (collision.tag == "Player") 
-            { 
-                collision.GetComponent<Snap>().ActiveSnap(targetDimension == collision.GetComponent<Snap>().GetNextDimension() ? 1 : -1); 
-                sonPortailAspire.PlayOneShot(sonPortailObjet[1], 0.1f); 
-            } 
-        } 
-    } 
- 
-    private void OnTriggerExit2D(Collider2D collision) 
-    { 
-        var objetTransfert = objectsReceived.Find(objet => objet.collider == collision); 
-        if (objetTransfert != null) 
-        { 
-            if (collision.tag == "Item") 
-            { 
-                if (FindProvenance(collision.transform) == objetTransfert.provenance) 
-                    collision.gameObject.layer = targetDimension + 9; 
-                else 
-                    collision.gameObject.layer = portailLinked.targetDimension + 9; 
-            } 
-            objectsReceived.Remove(objetTransfert); 
-            cameras[targetDimension].GetComponent<Camera>().cullingMask &= ~(1 << portailLinked.targetDimension + 12); // On retire le layer de transition du culling mask 
-            collision.GetComponent<SpriteRenderer>().sortingLayerID = SortingLayer.NameToID(LayerMask.LayerToName(collision.gameObject.layer)); 
-        } 
-    } 
- 
-    public void transferObject(ObjetTransfert objet) 
-    { 
-        objectsReceived.Add(objet); 
-    } 
- 
-    private Vector2 FindProvenance(Transform objetTransform) 
-    { 
-        Vector2 direction = objetTransform.position - transform.position; 
-        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) 
-            direction = direction.x > 0 ? Vector2.right : Vector2.left; 
-        else 
-            direction = direction.y > 0 ? Vector2.up : Vector2.down; 
- 
-        return direction; 
-    }*/
-}
-
-
-public class ObjetTransfert
-{
-    public Collider2D collider { get; set; }
-    public Vector2 provenance { get; set; }
-
-    public ObjetTransfert(Collider2D collider, Vector2 provenance)
-    {
-        this.collider = collider;
-        this.provenance = provenance;
-    }
 }
