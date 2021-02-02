b063b766c06858263b92af839dce56a066329976
LeviiOsa <hamai.sabine@gmail.com>
2021-02-02 11:13:21 +0100

    petite corrections

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject optionUI;
+    public GameObject collectableUI;
+    public GameObject quitterBDUI;
+
+    public GameObject firstOptionButton;
+    public GameObject firstCollectableButton;
+    public GameObject firstCQButton;
+    
     public void PlayGame()
     {
         SceneManager.LoadScene("Chapitre 1 - Niveau 1");
@@ -19,6 +28,28 @@ public class MainMenu : MonoBehaviour
     {
         Application.OpenURL("https://snaplejeu.fandom.com/fr/wiki/SNAP,_Le_Jeu");
 
+    }
+    public void CollectableMenu()
+    {
+        collectableUI.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstCollectableButton);
+
+    }
+    public void OptionMenu()
+    {
+        optionUI.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstOptionButton);
+    }
+    public void QuitterMenu()
+    {
+        quitterBDUI.SetActive(true);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstCQButton);
     }
 
 }
