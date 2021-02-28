d97c252b8b1c0565613904d25bfb00a3301d5745
LeviiOsa <hamai.sabine@gmail.com>
2021-02-28 11:13:15 +0100

    3d

---
+++ Assets/Level2Loader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2Loader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public float timer = 3f;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Sauter"))
+        {
+            LoadNextLevel();
+        }
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                LoadNextLevel();
+            }
+        }
+
+
+
+    }
+    public void LoadNextLevel()
+    {
+
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
