2dd81cb1a02124583dfdd373f8c31ebbee8460e6
LeviiOsa <hamai.sabine@gmail.com>
2021-03-01 14:18:51 +0100

    respawn dimension

--- Assets/Scripts/RespawnItem.cs
+++ Assets/Scripts/RespawnItem.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class RespawnItem : MonoBehaviour
 {
     private Vector3 itemPosition;
+    public LayerMask itemLayer;
+    public bool isDead;
 
 
     private void Start()
     {
+        isDead = false;
+
         if (transform.tag == "Item")
         {
             itemPosition =new Vector3( GetComponent<Transform>().position.x,GetComponent<Transform>().position.y, 0);
+            itemLayer = gameObject.layer;
         }
 
     }
@@ -19,7 +24,7 @@ public class RespawnItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T) || isDead)
         {
             RespawnPositionItem();
         }
@@ -28,6 +33,7 @@ public class RespawnItem : MonoBehaviour
     {
 
         transform.position = itemPosition;
+        gameObject.layer = itemLayer;
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,6 +42,7 @@ public class RespawnItem : MonoBehaviour
 
         if (collision.tag == "Dead")
         {
+            isDead = true;
             RespawnPositionItem();
         }
     }
