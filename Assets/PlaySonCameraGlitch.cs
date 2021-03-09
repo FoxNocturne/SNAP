515e2b94be6d4cc662e483614fa29e45ebc644ea
LeviiOsa <hamai.sabine@gmail.com>
2021-03-09 16:52:07 +0100

    sons

---
+++ Assets/PlaySonCameraGlitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySonCameraGlitch : MonoBehaviour
+{
+    AudioSource sonCameraEnclencher;
+    public AudioClip[] sonCamera;
+
+
+    void Start()
+    {
+        sonCameraEnclencher = GetComponent<AudioSource>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+            sonCameraEnclencher.PlayOneShot(sonCamera[0], 1f);
+
+
+        }
+    }
+
+    // Update is called once per frame
+  
+}
