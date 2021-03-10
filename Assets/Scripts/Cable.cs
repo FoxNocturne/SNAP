71afeb7a7c4354bed5a88c86dc39ab365990ca6d
Anthony HUREZ <anthonyhurez@yahoo.fr>
2021-03-10 12:08:23 +0100

    Ajout câbles

---
+++ Assets/Scripts/Cable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cable : ObjetActivable
+{
+    public Color couleurEteint;
+    public Color couleurAllume;
+
+
+    private void Update()
+    {
+        if (activators.Count != 0)
+            GetComponent<SpriteRenderer>().color = couleurAllume;
+        else
+            GetComponent<SpriteRenderer>().color = couleurEteint;
+    }
+
+    public override void Activation(GameObject activator)
+    {
+        activators.Add(activator);
+        //sonPorteEnclencher.PlayOneShot(sonPorte[0], 1f);
+    }
+
+    public override void Desactivation(GameObject activator)
+    {
+        activators.Remove(activator);
+        //sonPorteEnclencher.PlayOneShot(sonPorte[1], 1f);
+    }
+}
